290fdcee4e5dea02e6897522808ca98ba691e5f8
Sara Grankvist <sara.grankvist@centria.fi>
2026-04-26 23:26:59 +0300

    Updated Final Project

--- Final_Project/src/final_project.ino
+++ Final_Project/src/final_project.ino
@@ -1,32 +1,96 @@
 #include <ESP32Servo.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-#define SENSOR_ANALOG_PIN 2
+#define SENSOR_ANALOG_PIN 35
 #define SENSOR_DIGITAL_PIN 4
 #define BUZZ_PIN 15
-#define SERVO_PIN 16
+#define SERVO_PIN 32
 #define BUTTON_PIN 12
 
+// Wi-Fi credentials
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// MQTT broker configuration
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "saragrankv/gassensor";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//LCD and Servo configuration
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-Servo servo;
+Servo ventservo;
 bool resetAlarm = false;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(SENSOR_ANALOG_PIN, INPUT);
   pinMode(SENSOR_DIGITAL_PIN, INPUT);
   pinMode(BUZZ_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  servo.attach(SERVO_PIN);
-  servo.write(0);
+  ventservo.attach(SERVO_PIN);
+  ventservo.write(0);
   lcd.init();
   lcd.backlight();
-  delay(1000);
+  delay(500);
+
+  // WiFi connection
+  printMessage("Connecting WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    printMessage("...");
+  }
+  printMessage("Connected!");
+
+  // MQTT connection
+  client.setServer(mqtt_server, mqtt_port);
+  connectToMQTT();
+  delay(500);
+  lcd.clear();
+}
+
+void printMessage(String msg) {
+  // Sends status messages to both Serial monitor and LCD
+  Serial.println(msg);
+  lcd.setCursor(0,0);
+  lcd.print(msg);
+}
+
+void connectToMQTT() {
+  while (!client.connected()) {
+    printMessage("Connecting MQTT");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      printMessage("Connected!");
+    }
+    else {
+      printMessage("Failed:");
+      delay(500);
+      printMessage(String(client.state()));
+      printMessage("Retry in 5s...");
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
+  // Check MQTT connection
+  if(!client.connected()) {
+    connectToMQTT();
+  }
+  client.loop();
+
   // Read value from sensor, write to LCD
   int gasLevel = analogRead(SENSOR_ANALOG_PIN);
+  bool alarmOn = false;
   lcd.setCursor(0,0);
   lcd.print(gasLevel);
   // Check digital pin, sound alarm if LOW unless alarm has already been reset
@@ -34,18 +98,19 @@ void loop() {
   int threshold = digitalRead(SENSOR_DIGITAL_PIN);
   lcd.setCursor(0,1);
   if (threshold == LOW) {
+    alarmOn = true;
     lcd.setCursor(0,1);
     lcd.print("ALARM");
     if (!resetAlarm) {
-      tone(BUZZ_PIN, 500, 200);
+      tone(BUZZ_PIN, 500, 500);
     }
-    servo.write(180);
+    ventservo.write(180);
     delay(200);
   }
   else {
     lcd.setCursor(0,1);
     lcd.print("OK   ");
-    servo.write(0);
+    ventservo.write(0);
     delay(200);
   }
   int buttonPressed = digitalRead(BUTTON_PIN);
@@ -54,5 +119,8 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print("ALARM (muted)");
   }
-  delay(10); // this speeds up the simulation
+  // Publish to MQTT
+  String payload = "{\"reading\": " + String(gasLevel) + ", \"alarm\": " + String(!alarmOn) + "}";
+  client.publish(mqtt_topic, payload.c_str());
+  delay(1000);
 }
